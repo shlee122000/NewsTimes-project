584e89d4e0796d94cf337aaa4dafdd10e018dde9
shlee122000 <160861717+shlee122000@users.noreply.github.com>
2024-10-23 20:40:51 +0900

    NewTimes 웹페이지꾸미기 final_category

--- main.js
+++ main.js
@@ -1,18 +1,37 @@
-//const API_KEY ='a233707d11d84e4cbe6ccc12fce0c251';
-let newList = [];
+// const API_KEY ='a233707d11d84e4cbe6ccc12fce0c251';
+let newsList
+ = [];
+const menus = document.querySelectorAll(".menus button");
+// console.log("mmm", menus);
+menus.forEach(menu=>menu.addEventListener("click",(event)=>getNewsByCategory(event)));
+
 const getLatestNews = async()=>{
-    const url = new URL('https://newsapi.org/v2/top-headlines?country=us&apiKey=a233707d11d84e4cbe6ccc12fce0c251')
-    //const url = new URL('https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}');
+    const url = new URL('https://newsapi.org/v2/top-headlines?country=us&apiKey=a233707d11d84e4cbe6ccc12fce0c251');
+    // const url = new URL('https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}');
     // console.log("uuu", url);
     const response = await fetch(url);
-    console.log("rrrr", response);
+    // console.log("rrrr", response);
     const data = await response.json();
-    newList = data.articles;
+    newsList
+     = data.articles;
     render();
-    // console.log("dddd", newList);
+    // console.log("dddd", newsList);
 };
+
+const getNewsByCategory= async (event)=>{
+    // console.log("ddddd", newsList)
+    const category = event.target.textContent.toLowerCase();
+    console.log("category", category);
+    const url = new URL('https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=a233707d11d84e4cbe6ccc12fce0c251');
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.log("kkk", data);
+    newsList = data.articles;
+    render();
+};
+
 const render=()=> {
-    const newsHTML = newList.map((news)=>`<div class="row news">
+    const newsHTML = newsList.map((news)=>`<div class="row news">
     <div class="col-lg-4">
         <img class="news-img-size" 
             src=${news.urlToImage} />
@@ -29,8 +48,7 @@ const render=()=> {
     </div>
 </div>`
     ).join("");
-    // console.log("html", newsHTML);
-    newsHTML =                      
+    // console.log("html", newsHTML);                  
          
     document.getElementById("news-board").innerHTML=newsHTML;
 };
@@ -38,4 +56,6 @@ const render=()=> {
 getLatestNews();
 
 
-                
+// 1. 버튼들에 클릭이벤트 주기
+// 2. 카네고리별 뉴스 가져오기
+// 3. 그 뉴스 보여주기
