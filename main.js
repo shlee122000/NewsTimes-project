3924b84c8c600423f94e92546cd765f8371de9e4
shlee122000 <160861717+shlee122000@users.noreply.github.com>
2024-11-27 19:56:47 +0900

    NewTimes 웹페이지꾸미기 final_category_1

--- main.js
+++ main.js
@@ -1,63 +1,32 @@
 // const API_KEY ='a233707d11d84e4cbe6ccc12fce0c251';
-let newsList = [];
+let newsList
+ = [];
 const menus = document.querySelectorAll(".menus button");
-// console.log("mmm", menus);
-menus.forEach((menu)=>menu.addEventListener("click",(event)=>getNewsByCategory(event))
-);
-
-let url = new URL('https://newsapi.org/v2/top-headlines?country=us&apiKey=a233707d11d84e4cbe6ccc12fce0c251')
-
-let totalResults = 0
-let page = 1
-const pageSize=10
-const groupSize=5
-
-const getNews = async()=> {
-    try{
-        url.searchParams.set("page",page)  //  =>&page=page
-        url.searchParams.set("pageSize", pageSize);
-        const response = await fetch(url);    
-        const data = await response.json();
-        if(response.status===200){
-            if(data.articles.length===0) {
-                throw new Error("No result for this search");
-            }
-            newsList = data.articles;
-            totalResult = data.totalResults
-            render();
-            paginationRender()
-        }else{
-            throw new Error(data.message)
-        }
-        
-
-    }catch(error){
-        // console.log("error", error.message);
-        errorRender(error.Message)
-    }
-
-};
+console.log("mmm", menus);
+menus.forEach(menu=>menu.addEventListener("click",(event)=>getNewsByCategory(event)));
 
 const getLatestNews = async()=>{
-    url = new URL('https://newsapi.org/v2/top-headlines?country=us&apiKey=a233707d11d84e4cbe6ccc12fce0c251');
- 
-    getNews();
+    const url = new URL('https://newsapi.org/v2/top-headlines?country=us&apiKey=a233707d11d84e4cbe6ccc12fce0c251');
+    // const url = new URL('https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}');
+    console.log("uuu", url);
+    const response = await fetch(url);
+    console.log("rrrr", response);
+    const data = await response.json();
+    newsList = data.articles;
+    render();
+    console.log("dddd", newsList);
 };
 
-const getNewsByKeyword = async()=>{
-    const keyword = document.getElementById("search-input").value;
-    console.log("keyword", keyword);
-    url = new URL('https://newsapi.org/v2/top-headlines?country=us&category=q={keyword}&apiKey=a233707d11d84e4cbe6ccc12fce0c251');
-     
-    getNews();
-};
-const getNewsByCategory= async (event)=>{
-    // console.log("ddddd", newsList)
+const getNewsByCategory = async (event)=>{
+    console.log("ddddd", newsList);
     const category = event.target.textContent.toLowerCase();
     console.log("category", category);
-    url = new URL('https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=a233707d11d84e4cbe6ccc12fce0c251');
-    
-    getNews();
+    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=a233707d11d84e4cbe6ccc12fce0c251`);
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log("kkk", data);
+    newsList = data.articles;
+    render();
 };
 
 const render=()=> {
@@ -74,95 +43,18 @@ const render=()=> {
         <div>
             ${news.source.name} * ${news.publishedAt}
         </div>
+
     </div>
 </div>`
     ).join("");
     // console.log("html", newsHTML);                  
-
+         
     document.getElementById("news-board").innerHTML=newsHTML;
 };
 
-const errorRender = (errorMessage)=> {
-    const errorHTML =`<div class="alert alert-danger" role="alert"> 
-    ${errorMessage}
-    </div>`;
-     
-    document.getElementById("news-board").innerHTML=errorHTML
-};
-
-const paginationRender=()=>{
-    // totalResult,
-    // page
-    // pageSize
-    // groupSize
-    // totalPages
-    const totalPages = Math.ceil(totalResults/[ageSize])
-    // pageGroup
-    const pageGroup = Math.ceil(page/groupSize);
-    // lastPage
-    let lastPage = pageGroup * groupSize;
-    // 마지막 페이지 그룹이 그룹사이즈보다 작다 ?  lastpage = totalpage
-    if(lastPage>totalPage){
-        lastPage=totalPages
-    }
-    // firstPage
-    const firstPage = lastPage - (groupSize-1)<=0? 1: lastPage - (groupSize - 1);
-    // first~last
-    // totalPages
-
-    let paginationHTML = '<li class="page-item" on click="moveToPage(${page-1})"><a class="page-link" href="#">Previous</a></li>';
-
-    for(let i=firstPage; i<=lastPage; i++){
-        pagenationHTML+= '<li class="page-item" ${i===page? "active" : ""}" onclick="moveToPage(${i}"><a class="page-link">${i}</a></li>';
-    }
-    
-    for (let i = frstPage; i <= lastPage; i++) {
-        paginationHTML += `<li class="page-item ${
-            i === page ? "active" : ""
-        }" onclick=",moveToPage(${i})" > <a class="page-link">${i}</a></li>` ;     
-    }
-
-
-    paginationHTML += <li class="page-item" on click="moveToPage(${page+1})">
-      <a class="page-link" href="#">Next</a> </li>;
-    document.querySelector(".pagination").innerHTML=paginationHTML;
-
-//  <nav aria-label="Page navigation example">
-//   <ul class="pagination">
-//     <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-//     <li class="page-item"><a class="page-link" href="#">1</a></li>
-//     <li class="page-item"><a class="page-link" href="#">2</a></li>
-//     <li class="page-item"><a class="page-link" href="#">3</a></li>
-//     <li class="page-item"><a class="page-link" href="#">Next</a></li>
-//   </ul>
-// </nav> 
-};
-
-const moveToPage=(pageNum)=>{
-    console.log("movetopage", pageNum);
-    page = pageNum;
-    getNews();
-};
 getLatestNews();
 
 
 // 1. 버튼들에 클릭이벤트 주기
 // 2. 카네고리별 뉴스 가져오기
 // 3. 그 뉴스 보여주기
-
-
-
-
-// error 발생
-// let weight = 29
-// try{
-//     // 소스를 쓴다
-//     // 이안에 에러가 발생하면
-//     if(weight<30){
-//         throw new Error("당신은 너무 말랐어")
-//     }
-    
-// }catch(error){
-// // catch가 error를 잡아준다
-// console.log("내가 잡은 에러는",error.message)
-// }
